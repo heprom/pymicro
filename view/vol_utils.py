ed6ad976550f0fe49b73eaadf32863278572f2de
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2014-05-02 11:02:08 +0200

    corrected prefix variable in grey level histogram plotting

--- view/vol_utils.py
+++ view/vol_utils.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 '''
   Compute and plot the gray level histogram of the provided data array.
 '''
-def hist(data, nb_bins=256, show=True, save=False):
+def hist(data, nb_bins=256, show=True, save=False, prefix='data'):
   print 'computing gray level histogram'
   hist, bin_edges = np.histogram(data, bins=nb_bins, range=(0,255))
   bin_centers = 0.5*(bin_edges[:-1] + bin_edges[1:])

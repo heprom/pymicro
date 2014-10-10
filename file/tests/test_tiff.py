a2b90e27be0a2ecbd545d9629277665be58707ae
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2014-10-10 11:02:38 +0200

    added test for tif file

---
+++ file/tests/test_tiff.py
@@ -0,0 +1,24 @@
+import unittest
+import numpy as np
+import os
+from file.tifffile import imsave, imread
+
+class TiffTests(unittest.TestCase):
+
+  def setUp(self):
+    print 'testing the Tifffile module'
+    self.data = (255*np.random.rand(5, 301, 219)).astype(np.uint8)
+    imsave('temp.tif', self.data)
+
+  def test_imread(self):
+    image = imread('temp.tif')
+    print 'image size is', np.shape(image)
+    np.testing.assert_array_equal(image, self.data)
+
+  def tearDown(self):
+    os.remove('temp.tif')
+
+if __name__ == '__main__':
+  unittest.main()
+
+

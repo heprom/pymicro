2204068be9ce33711854b59a30619770d0a0c361
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2014-10-21 13:36:21 +0200

    added new method show_and_save for a 2d image

--- view/vol_utils.py
+++ view/vol_utils.py
@@ -1,7 +1,37 @@
 import numpy as np
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, cm
 from matplotlib.colors import colorConverter
 import matplotlib as mpl
+
+def show_and_save(image, name, dpi=100, colormap=cm.gray, verbose=False):
+  '''Save a 2D image with pyplot.
+  
+  This function displays a 2D numpy array (or a slice of a 3D array)
+  using pyplot and save it to the disk as a png image.
+
+  *Parameters*
+  
+  **image**: the 2d data array to ssho and save.
+  
+  **name**: a string to use as the file name (without the extension)
+  
+  **dpi**: image resolution (default 100).
+  
+  **colormap**: the colormap tp use in matplotlib format (default gray).
+  
+  **verbose**: boolean to enable verbose mode (default False).
+  '''
+  (im_size_y, im_size_x) = image.shape
+  plt.figure(figsize=(im_size_x/float(dpi), im_size_y/float(dpi)))
+  if verbose:
+    print('image size is', image.shape)
+    print('figure size is:', np.array(image.shape)/float(dpi))
+  plt.axis('off')
+  plt.hold('off')
+  plt.subplots_adjust(top=1, bottom=0, left=0, right=1)
+  plt.imshow(image, cmap=colormap, interpolation='nearest')
+  plt.savefig(name + '.png', format='png')
+  plt.close()
 
 '''
   Compute and plot the gray level histogram of the provided data array.

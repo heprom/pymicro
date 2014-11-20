7dbd525a6e7001c95d6649400b56be93f3548a9f
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2014-11-20 16:15:30 +0100

    more __init__ stuff

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,1 +1,13 @@
+"""A package regrouping some graphical utilities to help working with 
+   synchrotron data.
+
+.. moduleauthor:: Henry Proudhon <henry.proudhon@gmail.com>
+
+"""
+
+__version__  = '0.1.0'
+from wxImageViewer import ImageViewer
+
+def start():
+    'This starts the apps package...'
 

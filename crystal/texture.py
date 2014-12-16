9264ef049dd3473506121e224339d01150a514da
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2014-12-16 14:19:16 +0100

    many docstrings fixes and new function plot_sst

--- crystal/texture.py
+++ crystal/texture.py
@@ -4,22 +4,44 @@ from pymicro.crystal.microstructure import Orientation, Grain, Microstructure
 from matplotlib import pyplot as plt, colors, cm
 
 class PoleFigure:
-
-  '''
-  Create an empty PoleFigure object associated with an empty Microstructure.
+  '''A class to handle pole figures.
   
-  structure: crystal structure (only 'cubic' for now)
-  proj: projection type, can be either 'stereo' (default) or 'flat'
-  mksize: marker size as displayed on the plots
-  verbose: verbose mode
-  color_by_grain_id: color the spot on the pole figure with grain color
-  pflegend: show the legend (only if color_by_grain_id is active)
+  Pole figure is a popular tool to plot multiple crystal orientations, 
+  either in the sample coordinate system (direct pole figure) or 
+  alternatively plotting a particular direction in the crystal 
+  coordinate system (inverse pole figure).
   '''
-  def __init__(self, structure='cubic', proj='stereo', \
-               microstructure = Microstructure()):
+
+  def __init__(self, microstructure=None, structure='cubic', proj='stereo'):
+    '''
+    Create an empty PoleFigure object associated with an empty Microstructure.
+    
+    *Parameters*
+
+    **microstructure**: the microstructure containing the collection of orientations to plot (None by default)
+
+    **structure**: the crystal structure
+
+    .. warning::
+
+       Right now only the cubic crystal structure is supported.
+
+    **proj**: projection type, can be either 'stereo' (default) or 'flat'
+    
+    **mksize**: marker size as displayed on the plots (12 pts by default)
+    
+    **verbose**: verbose mode (False by default)
+    
+    **color_by_grain_id**: color the spot on the pole figure with grain color (False by default)
+    
+    **pflegend**: show the legend (only if color_by_grain_id is active, False by default)
+    '''
     self.structure = structure
     self.proj = proj
-    self.microstructure = microstructure
+    if microstructure:
+      self.microstructure = microstructure
+    else:
+      self.microstructure = Microstructure()
     self.mksize = 12
     self.verbose = False
     self.color_by_grain_id = False
@@ -37,8 +59,26 @@ class PoleFigure:
     else:
       raise TypeError('unsupported crystal structure', structure)
 
-  '''Plot and save a picture with both direct and inverse pole figures...'''
   def plot_pole_figures(self):
+    '''Plot and save a picture with both direct and inverse pole figures.
+    
+    ::
+    
+      micro = Microstructure(name = 'AlLi_sam8')
+      micro.grains.append(Grain(11, Orientation.from_euler(np.array([262.364, 16.836, 104.691]))))
+      pf = PoleFigure(structure='cubic', proj='stereo', microstructure=micro)
+      pf.color_by_grain_id = True
+      pf.pflegend = True # this works well for a few grains
+      pf.plot_pole_figures()
+    
+    .. figure:: _static/AlLi_sam8_pole_figure.png
+        :width: 750 px
+        :height: 375 px
+        :alt: AlLi_sam8_pole_figure
+        :align: center
+
+        A 111 pole figure plotted for a single crystal orientation.
+    '''
     fig = plt.figure(figsize=(10,5))
     # direct PF
     ax1 = fig.add_subplot(121, aspect='equal')
@@ -48,8 +88,8 @@ class PoleFigure:
     self.plot_ipf(ax = ax2)
     plt.savefig(self.microstructure.name + '_pole_figure.pdf',format='pdf')
 
-  '''Helper function to plot a crystal direction.'''
   def plot_crystal_dir(self, c_dir, mk='o', col='k', ax=None, ann=False, lab=''):
+    '''Helper function to plot a crystal direction.'''
     if c_dir[2] < 0: c_dir *= -1 # make unit vector have z>0
     if self.proj == 'flat':
       cp = c_dir
@@ -66,8 +106,8 @@ class PoleFigure:
       ax.annotate(c_dir.view(), (cp[0], cp[1]-0.1), xycoords='data',
         fontsize=8, horizontalalignment='center', verticalalignment='center')
 
-  '''Helper function to plot a curve between two crystal directions.'''
-  def plot_line_between_crystal_dir(self, c1, c2, ax=None, steps=11):
+  def plot_line_between_crystal_dir(self, c1, c2, ax=None, steps=11, col='g'):
+    '''Helper function to plot a curve between two crystal directions.'''
     path = np.zeros((steps,2), dtype=float)
     for j,i in enumerate(np.linspace(0,1,steps)):
       ci = i*c1 + (1-i)*c2
@@ -77,18 +117,18 @@ class PoleFigure:
         ci /= ci[2]
       path[j,0] = ci[0]
       path[j,1] = ci[1]
-    ax.plot(path[:,0], path[:,1], 'g-', markersize=self.mksize)
+    ax.plot(path[:,0], path[:,1], color=col, markersize=self.mksize)
 
-  '''Helper function to plot the background of the pole figure. '''
   def plot_pf_background(self, ax):
+    '''Helper function to plot the background of the pole figure. '''
     an = np.linspace(0,2*np.pi,100)
     plt.hold('on')
     ax.plot(np.cos(an), np.sin(an), 'k-')
     ax.plot([-1,1], [0,0], 'k-')
     ax.plot([0,0], [-1,1], 'k-')
 
-  ''' Create the direct pole figure. '''
   def plot_pf(self, ax=None, mk='o', col='k', ann=False):
+    '''Create the direct pole figure. '''
     self.plot_pf_background(ax)
     ax.annotate('x', (1.01, 0.0), xycoords='data',
       fontsize=16, horizontalalignment='left', verticalalignment='center')
@@ -114,8 +154,47 @@ class PoleFigure:
     ax.axis('off')
     ax.set_title('direct %s projection' % self.proj)
 
-  ''' Create the inverse pole figure for direction Z. '''
+  def sst_symmetry(self, z_rot):
+    '''Perform symmetry according to the unit SST triangle.'''
+    if z_rot[0] < 0: z_rot[0] *= -1.
+    if z_rot[1] < 0: z_rot[1] *= -1.
+    if z_rot[1] > z_rot[0]:
+      z_rot[1], z_rot[0] = z_rot[0], z_rot[1]
+    return z_rot
+    
+  def plot_sst(self, ax=None, mk='s', col='r', ann=False):
+    ''' Create the inverse pole figure in the unit standard triangle. 
+    '''
+    c001 = np.array([0,0,1])
+    c101 = np.array([1,0,1])
+    c111 = np.array([1,1,1])
+    self.plot_line_between_crystal_dir(c001, c101, ax=ax)
+    self.plot_line_between_crystal_dir(c001, c111, ax=ax)
+    self.plot_line_between_crystal_dir(c101, c111, ax=ax)
+    # now plot the sample z-axis
+    for grain in self.microstructure.grains:
+      B = grain.orientation_matrix()
+      # compute z axis and apply SST symmetry
+      z_rot = self.sst_symmetry(B.dot(self.z))
+      print z_rot
+      self.plot_crystal_dir(z_rot, mk=mk, col=col, ax=ax, ann=ann)
+      if self.verbose: print 'plotting ',self.z,' in sample CS:',z_rot
+    ax.axis('off')
+    ax.set_title('inverse %s projection, unit standard triangle' % self.proj)
+    
   def plot_ipf(self, ax=None, mk='s', col='r', ann=False):
+    ''' Create the inverse pole figure for direction Z. 
+    
+    Parameters:
+    
+    **ax**: a reference to a pyplot ax to draw the figure
+    
+    **mk**: marker type (square by default)
+    
+    **col**: marker color (red by default)
+    
+    **ann**: draw annotation near the crystal direction (False by default)
+    '''
     self.plot_pf_background(ax)
     for c in self.c111s:
       for i in range(3):
@@ -140,34 +219,3 @@ class PoleFigure:
     ax.axis([-1.1,1.1,-1.1,1.1])
     ax.axis('off')
     ax.set_title('inverse %s projection' % self.proj)
-
-if __name__ == '__main__':
-  '''
-  The following is a counter example for OrientationMatrix2Euler.
-  Indeed it gives g[3,3] = 1 so according to DCTdoc_Crystal_orientation_descriptions.pdf
-  we should have Phi=0.0 and phi1=phi2=10.0 which is obviously not the case here.
-  '''
-  grain = Grain(1, Orientation.from_euler(np.array([10.0, 0.0, 0.0])))
-  #grain = Grain(1, Orientation.from_euler(np.array([142.8, 32.0, 214.4])))
-  euler = grain.orientation.euler # those are computed by OrientationMatrix2Euler
-  grain2 = Grain(2, Orientation.from_euler(euler))
-  print grain.orientation
-  print grain2.orientation
-  micro = Microstructure()
-  micro.grains.append(grain)
-  micro.grains.append(grain2)
-  pf = PoleFigure(structure='cubic', proj='flat', microstructure=micro)
-  print pf.c111s
-  fig = plt.figure(figsize=(12,5))
-  ax1 = fig.add_subplot(131, aspect='equal')
-  pf.plot_pf(ax = ax1)
-  ax2 = fig.add_subplot(132, aspect='equal')
-  pf.proj = 'flat'
-  pf.plot_ipf(ax = ax2)
-  # change projection mode to stereo
-  pf.proj = 'stereo'
-  ax3 = fig.add_subplot(133, aspect='equal')
-  pf.plot_ipf(ax = ax3)
-  plt.subplots_adjust(top=1, bottom=0, left=0, right=1)
-  plt.savefig('ipf_full.pdf',format='pdf')
-  plt.show()

067e52b144f60cfa00fb5b8361c464f930f75378
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2015-01-12 09:19:12 +0100

    moved tests for HklPlane in single file

--- crystal/tests/test_Lattice.py
+++ crystal/tests/test_Lattice.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from crystal.lattice import Lattice, HklPlane
+from crystal.lattice import Lattice
 
 class LatticeTests(unittest.TestCase):
 
@@ -20,16 +20,6 @@ class LatticeTests(unittest.TestCase):
     l = Lattice.cubic(0.5)
     self.assertAlmostEqual(l.volume(), 0.125)
   
-  def test_HklPlane(self):
-    p = HklPlane(1,1,1)
-    n = p.normal()
-    self.assertEqual(np.linalg.norm(n), 1)
-  
-  def test_bragg_angle(self):
-    l = Lattice.cubic(0.287) # FCC iron
-    hkl = HklPlane(2, 0, 0, l) # 200 reflection at 8 keV is at 32.7 deg
-    self.assertAlmostEqual(hkl.bragg_angle(8), 0.5704164) 
-    
   def test_slip_systems(self):
     print 'test_slip_systems'
     print Lattice.get_slip_systems(plane_type='111')

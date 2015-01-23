96b55385f69a433ff09aeda57248765b42857c56
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2015-01-23 18:10:06 +0100

    added new test for vtk numpy array

---
+++ view/tests/test_vtk_numpy_array.py
@@ -0,0 +1,31 @@
+import unittest
+import numpy as np
+import vtk
+from vtk.util import numpy_support
+
+class VtkNumpyArrayTests(unittest.TestCase):
+
+  def setUp(self):
+    print 'testing numpy to vtk array conversion'
+    self.data = np.linspace(0,20*10*5-1,1000).reshape((20,10,5)).astype(np.uint8)
+    self.vtk_data_array = numpy_support.numpy_to_vtk(np.ravel(self.data, order='F'), deep=1)
+
+  def test_data_size(self):
+    self.assertEqual(np.size(self.data), self.vtk_data_array.GetSize())
+  
+  def test_data_type(self):
+    self.assertEqual(self.vtk_data_array.GetDataTypeAsString(), 'unsigned char')
+
+  def test_data_access(self):
+    size = self.data.shape
+    numpy_val = self.data[10, 5, 3]
+    vtk_val = self.vtk_data_array.GetValue(3*size[0]*size[1] + 5*size[0] + 10)
+    self.assertEqual(numpy_val, vtk_val)
+
+  def tearDown(self):
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
+
+

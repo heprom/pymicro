ffdaa04dc1216a83b2bc30abe77fdbad54569797
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2015-09-04 17:28:14 +0200

    updated example to use the new animation framework

--- examples/animation/grain_hkl_anim_3d.py
+++ examples/animation/grain_hkl_anim_3d.py
@@ -2,20 +2,20 @@ import os, vtk
 import numpy as np
 from scipy import ndimage
 
-from pymicro.file.file_utils import edf_read
+from pymicro.file.file_utils import HST_read
 from pymicro.view.vtk_utils import *
-from pymicro.view.vtk_anim import vtkRotateActorAroundZAxis
+from pymicro.view.vtk_anim import vtkAnimationScene, vtkRotateActorAroundZAxis
 from pymicro.crystal.lattice import HklPlane
 from pymicro.crystal.microstructure import Orientation, Grain
 
-data_dir = os.path.join('../rawdata')
+data_dir = os.path.join('../data')
 scan = 'grain1_112x112x121_uint8.raw'
 im_file = os.path.join(data_dir, scan)
 
 print('create a python Grain object')
 orientation = Orientation.from_rodrigues(np.array([0.3889, -0.0885, 0.3268]))
 grain = Grain(1, orientation)
-grain_data = edf_read(im_file, header_size=0, autoparse_filename= True, verbose=True)
+grain_data = HST_read(im_file, autoparse_filename=True, verbose=True)
 grain.position = ndimage.measurements.center_of_mass(grain_data, grain_data)
 grain.volume = ndimage.measurements.sum(grain_data) # label is 1.0 here
 grain.add_vtk_mesh(grain_data, contour=False)
@@ -51,18 +51,15 @@ iren.SetRenderWindow(renWin)
 renWin.Render()
 iren.Initialize()
 
+scene = vtkAnimationScene(ren, ren_size)
+scene.save_image = True
+scene.timer_incr = 10
+scene.time_anim_ends = 360
+scene.prefix = name
 print('initialize the a vtkRotateActorAroundZAxis instance')
 anim_actor = vtkRotateActorAroundZAxis(0)
 anim_actor.actor = grain_with_planes
 anim_actor.actor_position = grain.position
-anim_actor.save_image = True
-anim_actor.timer_incr = 10
 anim_actor.time_anim_ends = 360
-if anim_actor.save_image:
-  print('images will be saved to folder %s' % name)
-  if not os.path.exists(name):
-    os.mkdir(name) # create a folder to store the images
-  anim_actor.prefix = name
-iren.AddObserver('TimerEvent', anim_actor.execute)
-timerId = iren.CreateRepeatingTimer(100); # time in ms
-iren.Start()
+scene.add_animation(anim_actor)
+scene.render()

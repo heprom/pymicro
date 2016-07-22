d8d680ad3d2d52463b0855e9d82637fc03766020
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2016-07-23 01:16:11 +0200

    $message

--- crystal/tests/test_Lattice.py
+++ crystal/tests/test_Lattice.py
@@ -26,6 +26,19 @@ class LatticeTests(unittest.TestCase):
       self.assertAlmostEqual(al._lengths[i], 0.40495)
       self.assertEqual(al._angles[i], 90.0)
   
+  def test_reciprocal_lattice(self):
+    Mg2Si = Lattice.from_parameters(1.534, 0.405, 0.683, 90., 106., 90.)
+    [astar, bstar, cstar] = Mg2Si.reciprocal_lattice()
+    self.assertAlmostEqual(astar[0], 0.678, 3)
+    self.assertAlmostEqual(astar[1], 0., 3)
+    self.assertAlmostEqual(astar[2], 0., 3)
+    self.assertAlmostEqual(bstar[0], 0., 3)
+    self.assertAlmostEqual(bstar[1], 2.469, 3)
+    self.assertAlmostEqual(bstar[2], 0., 3)
+    self.assertAlmostEqual(cstar[0], 0.420, 3)
+    self.assertAlmostEqual(cstar[1], 0., 3)
+    self.assertAlmostEqual(cstar[2], 1.464, 3)
+
 class HklDirectionTests(unittest.TestCase):
 
   def setUp(self):
@@ -40,6 +53,26 @@ class HklDirectionTests(unittest.TestCase):
     self.assertAlmostEqual(d111.angle_with_direction(d110)*180/np.pi, 35.26, 2)
     self.assertAlmostEqual(d111.angle_with_direction(dm111)*180/np.pi, 70.528, 2)
 
+  def test_tetragonal_direction(self):
+    bct = Lattice.body_centered_tetragonal(0.28, 0.40)
+    d111 = HklDirection(1, 1, 1, bct)
+    d110 = HklDirection(1, 1, 0, bct)
+    self.assertAlmostEqual(d111.direction()[0], 0.49746834, 5)
+    self.assertAlmostEqual(d111.direction()[1], 0.49746834, 5)
+    self.assertAlmostEqual(d111.direction()[2], 0.71066905, 5)
+    self.assertAlmostEqual(d110.direction()[0], 0.707106781, 5)
+    self.assertAlmostEqual(d110.direction()[1], 0.707106781, 5)
+    self.assertAlmostEqual(d110.direction()[2], 0.0, 5)
+
+  def test_tetragonal_direction2(self):
+    ZrO2 = Lattice.tetragonal(0.364, 0.527)
+    d = HklDirection(1, 1, 1, ZrO2)
+    target = np.array([1., 1., 1.448])
+    target /= np.linalg.norm(target)
+    self.assertAlmostEqual(d.direction()[0], target[0], 4)
+    self.assertAlmostEqual(d.direction()[1], target[1], 4)
+    self.assertAlmostEqual(d.direction()[2], target[2], 4)
+
 class HklPlaneTests(unittest.TestCase):
 
   def setUp(self):
@@ -50,6 +83,43 @@ class HklPlaneTests(unittest.TestCase):
     n = p.normal()
     self.assertEqual(np.linalg.norm(n), 1)
   
+  def test_HklPlane_normal(self):
+    ZrO2 = Lattice.tetragonal(3.64, 5.27)
+    p = HklPlane(1, 1, 1, ZrO2)
+    n = p.normal()
+    self.assertAlmostEqual(n[0], 0.635, 3)
+    self.assertAlmostEqual(n[1], 0.635, 3)
+    self.assertAlmostEqual(n[2], 0.439, 3)
+
+  def test_110_normal_monoclinic(self):
+    '''This test comes from 
+    http://www.mse.mtu.edu/~drjohn/my3200/stereo/sg5.html
+    corrected for a few errors in the html page.
+    '''  
+    Mg2Si = Lattice.from_parameters(1.534, 0.405, 0.683, 90., 106., 90.)
+    a = Mg2Si.matrix[0]
+    b = Mg2Si.matrix[1]
+    c = Mg2Si.matrix[2]
+    self.assertAlmostEqual(a[0], 1.475, 3)
+    self.assertAlmostEqual(a[1], 0., 3)
+    self.assertAlmostEqual(a[2], -0.423, 3)
+    self.assertAlmostEqual(b[0], 0., 3)
+    self.assertAlmostEqual(b[1], 0.405, 3)
+    self.assertAlmostEqual(b[2], 0., 3)
+    self.assertAlmostEqual(c[0], 0., 3)
+    self.assertAlmostEqual(c[1], 0., 3)
+    self.assertAlmostEqual(c[2], 0.683, 3)
+    p = HklPlane(1, 1, 1, Mg2Si)
+    Gc = p.scattering_vector()
+    self.assertAlmostEqual(Gc[0], 1.098, 3)
+    self.assertAlmostEqual(Gc[1], 2.469, 3)
+    self.assertAlmostEqual(Gc[2], 1.464, 3)
+    self.assertAlmostEqual(p.interplanar_spacing(), 0.325, 3)
+    Ghkl = np.dot(Mg2Si.matrix, Gc)
+    self.assertEqual(Ghkl[0], 1.) # h
+    self.assertEqual(Ghkl[1], 1.) # k
+    self.assertEqual(Ghkl[2], 1.) # l
+    
   def test_scattering_vector(self):
     Fe_fcc = Lattice.face_centered_cubic(0.287) # FCC iron
     hkl = HklPlane(2, 0, 0, Fe_fcc)

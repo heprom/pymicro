2fc2e4a67f7982ecab1c33c2d6198813220e635d
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2016-08-07 07:04:52 +0200

    updated misorientation tests

--- crystal/tests/test_Microstructure.py
+++ crystal/tests/test_Microstructure.py
@@ -27,13 +27,17 @@ class OrientationTests(unittest.TestCase):
   def test_misorientation_angle(self):
     o1 = Orientation.from_euler((0., 0., 0.))
     o2 = Orientation.from_euler((60., 0., 0.))
-    self.assertAlmostEqual(180/np.pi*o1.misorientation_angle(o2, crystal_structure='none'), 60)
-    self.assertAlmostEqual(180/np.pi*o1.misorientation_angle(o2, crystal_structure='cubic'), 30)
+    self.assertAlmostEqual(180/np.pi*o1.disorientation(o2, crystal_structure='none')[0], 60)
+    self.assertAlmostEqual(180/np.pi*o1.disorientation(o2, crystal_structure='cubic')[0], 30)
 
   def test_misorientation_axis(self):
     o1 = Orientation.copper()
     o2 = Orientation.s3()
-    self.assertAlmostEqual(180/np.pi*o1.misorientation_angle(o2, crystal_structure='none'), 19.38, 2) # check value of 19.576
+    (angle, axis, axis_xyz) = o1.disorientation(o2, crystal_structure='none')
+    self.assertAlmostEqual(180/np.pi*angle, 19.38, 2) # check value of 19.576
+    val = np.array([-0.71518544, -0.60383062, -0.35199199])
+    for i in range(3):
+      self.assertAlmostEqual(axis[i], val[i], 6)
     
   def test_Bragg_condition(self):
     al = Lattice.from_symbol('Al')

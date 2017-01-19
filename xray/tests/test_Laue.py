045fd50e6a5498c9ee4af70cce332a55412604c2
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2017-01-19 03:22:37 +0100

    new test for select_lambda method

--- xray/tests/test_Laue.py
+++ xray/tests/test_Laue.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 from pymicro.crystal.lattice import Lattice, HklDirection, HklPlane, SlipSystem
 from pymicro.crystal.microstructure import Orientation
+from pymicro.xray.laue import select_lambda
 
 class LaueTests(unittest.TestCase):
 
@@ -9,6 +10,8 @@ class LaueTests(unittest.TestCase):
     print 'testing the Lattice class'
 
   def test_angle_zone(self):
+    '''Verify the angle between X and a particular zone axis expressed 
+    in (X, Y, Z), given a crystal orientation.'''
     ni = Lattice.from_symbol('Ni')
     # euler angles in degrees
     phi1 = 89.4
@@ -23,6 +26,15 @@ class LaueTests(unittest.TestCase):
       ZA *= -1 # make sur the ZA vector is going forward
     psi0 = np.arccos(np.dot(ZA, np.array([1., 0., 0.])))
     self.assertAlmostEqual(psi0*180/np.pi, 9.2922, 3)
+
+  def test_select_lambda(self):
+    '''Verify the wavelength diffracted by a given hkl plane.'''
+    ni = Lattice.from_symbol('Ni')
+    orientation = Orientation.cube()
+    hkl = HklPlane(-1, -1, -1, ni)
+    (the_lambda, theta) = select_lambda(hkl, orientation)
+    self.assertAlmostEqual(the_lambda, 5.277, 3)
+    self.assertAlmostEqual(theta*180/np.pi, 35.264, 3)
 
 if __name__ == '__main__':
   unittest.main()

96e0c5963a51b236ced499b9015e321823807f4b
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2017-02-15 19:33:08 +0100

    new fil_utils tests

--- file/tests/test_tiff.py
+++ file/tests/test_tiff.py
@@ -1,7 +1,37 @@
 import unittest
 import numpy as np
 import os
+from pymicro.file.file_utils import *
 from pymicro.external.tifffile import imsave, imread
+
+
+class file_utils_Tests(unittest.TestCase):
+    def setUp(self):
+        print 'testing the file_utils module'
+        self.data = (255 * np.random.rand(20, 30, 10)).astype(np.uint8)
+        HST_write(self.data, 'temp_20x30x10_uint8.raw')
+
+    def test_HST_info(self):
+        dims = HST_info('temp_20x30x10_uint8.raw.info')
+        self.assertEqual(dims[0], 20)
+        self.assertEqual(dims[1], 30)
+        self.assertEqual(dims[2], 10)
+
+    def test_HST_read(self):
+        data = HST_read('temp_20x30x10_uint8.raw', autoparse_filename=True)
+        self.assertEqual(data.shape[0], 20)
+        self.assertEqual(data.shape[1], 30)
+        self.assertEqual(data.shape[2], 10)
+
+    def test_HST_read_mmap(self):
+        data = HST_read('temp_20x30x10_uint8.raw', autoparse_filename=True, mmap=True)
+        self.assertEqual(data.shape[0], 20)
+        self.assertEqual(data.shape[1], 30)
+        self.assertEqual(data.shape[2], 10)
+
+    def tearDown(self):
+        os.remove('temp_20x30x10_uint8.raw')
+        os.remove('temp_20x30x10_uint8.raw.info')
 
 
 class TiffTests(unittest.TestCase):

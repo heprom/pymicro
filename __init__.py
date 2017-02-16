f184105767893985b14912917780345048a83b49
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2017-02-16 17:32:35 +0100

    corrected typo

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-"""A package to work with materials microstructures and 3d data sets
+"""A package to work with material microstructures and 3d data sets
 
 .. moduleauthor:: Henry Proudhon <henry.proudhon@mines-paristech.fr>
 

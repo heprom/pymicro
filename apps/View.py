f2acfe4de718498bfda5716ac37380a485d3a62f
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2017-04-05 00:26:56 +0200

    added docstring for class View

--- apps/View.py
+++ apps/View.py
@@ -10,12 +10,26 @@ from pymicro.crystal.lattice import Lattice
 
 
 class View:
+    """Quick visualisation of various instances at use in pymicro.
+
+    This class provides a way to quickly visualise an instance of a particular class.
+    A 3D scene is then created with default settings and the actor added to the scene.
+    The following types are supported:
+
+     * Grain: the `grain_3d` method is called to create the actor;
+     * Orientation: A cube is created and rotated according to the passive orientation matrix;
+     * Lattice: the `lattice_3d` method is called to create the actor;
+     * numpy array: the `show_array` method is called to create the actor;
+     * vtk actor: the actor is directly added to the scene.
+
+    The key_pressed_callback is activated so it is possible to save an image using the 's' key.
+    """
+
     def __init__(self, args):
-        '''Init a View window.'''
+        '''Init a new View window.'''
         #print(args)
         # create the 3D scene
         s3d = Scene3D(display=True, ren_size=(800, 800))
-        args_type = type(args)
         if isinstance(args, list):
             if len(args) == 1:
                 print('Please specify the file representing the 3D object to view')
@@ -63,7 +77,7 @@ class View:
         cam = setup_camera(size)
         cam.SetFocalPoint(0.5 * (bounds[0] + bounds[1]), 0.5 * (bounds[2] + bounds[3]), 0.5 * (bounds[4] + bounds[5]))
         s3d.set_camera(cam)
-        s3d.render()
+        s3d.render(key_pressed_callback=True)
 
 
 if __name__ == "__main__":

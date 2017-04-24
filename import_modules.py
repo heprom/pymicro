dc12eebfd6af0fdcb4fa5aeeffe4d9832afd3365
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2017-04-24 22:13:27 +0200

    update imports to match the refactoring of the fitting module into the xray package

--- import_modules.py
+++ import_modules.py
@@ -7,10 +7,10 @@ from pymicro.crystal.lattice import *
 from pymicro.crystal.microstructure import *
 from pymicro.crystal.texture import *
 from pymicro.file.file_utils import *
-from pymicro.math.fitting import *
 from pymicro.view.scene3d import *
 from pymicro.view.vtk_utils import *
 from pymicro.view.vtk_anim import *
 from pymicro.xray.detectors import *
 from pymicro.xray.xray_utils import *
+from pymicro.xray.fitting import *
 from pymicro.xray.laue import *

689b3900f6b233d5abec7a25b5516459db91fa5f
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2017-05-04 00:43:58 +0200

    new gnomonic projection function

--- xray/laue.py
+++ xray/laue.py
@@ -166,7 +166,7 @@ def compute_Laue_pattern(orientation, detector, hklplanes=None, spectrum=None, s
         K = diffracted_vector(hkl, orientation)
         if K is None or np.dot([1., 0., 0.], K) == 0:
             continue  # skip diffraction // to the detector
-        R = detector.project_along_direction((0., 0., 0.), K)
+        R = detector.project_along_direction(K, origin=[0., 0., 0.])
         (u, v) = detector.lab_to_pixel(R)
         if u >= 0 and u < detector.size[0] and v >= 0 and v < detector.size[1]:
             print('diffracted beam will hit the detector at (%.3f, %.3f) mm or (%d, %d) pixels' % (R[1], R[2], u, v))
@@ -181,6 +181,81 @@ def compute_Laue_pattern(orientation, detector, hklplanes=None, spectrum=None, s
         detector.data = np.invert(detector.data)
     return detector.data
 
+def gnomonic_projection(detector):
+    '''This function carries out the gnomonic projection of the detector image.
+    
+    The data must be of uint8 type (between 0 and 255) with diffraction spots equals to 255.
+    The function create a new detector instance (think of it as a virtual detector) located at the same position 
+    as the given detector and with an inverse pixel size. The gnomonic projection is stored into this new detector data.
+    
+    :param RegArrayDetector2d detector: the detector instance with the data from which to compute the projection.
+    :returns RegArrayDetector2d gnom: A virtual detector with the gnomonic projection as its data.
+    '''
+    assert detector.data.dtype == np.uint8
+    dif = detector.data == 255
+    u = np.linspace(0, detector.size[0] - 1, detector.size[0])
+    v = np.linspace(0, detector.size[1] - 1, detector.size[1])
+    vv, uu = np.meshgrid(v, u)
+    r_pxl = np.sqrt((uu - detector.ucen) ** 2 + (vv - detector.vcen) ** 2)  # pixel
+    r = r_pxl * detector.pixel_size  # distance from the incident beam to the pxl in mm
+    theta = 0.5 * np.arctan(r / detector.ref_pos[0])  # bragg angle rad
+    p = detector.ref_pos[0] * np.tan(np.pi / 2 - theta)  # distance from the incident beam to the gnomonic projection mm
+
+    # compute gnomonic projection coordinates in mm
+    u_dif = (uu[dif] - detector.ucen) * detector.pixel_size  # mm, wrt detector center
+    v_dif = (vv[dif] - detector.vcen) * detector.pixel_size  # mm, wrt detector center
+    ug_mm = - u_dif * p[dif] / r[dif]  # mm, wrt detector center
+    vg_mm = - v_dif * p[dif] / r[dif]  # mm, wrt detector center
+    print('first projected spot in uv (mm):', (ug_mm[0], vg_mm[0]))
+    print('last projected spot in uv (mm):', (ug_mm[-1], vg_mm[-1]))
+    print('first projected vector in XYZ (mm):', (detector.ref_pos[0], -ug_mm[0], -vg_mm[0]))
+    print('last projected vector in XYZ (mm):', (detector.ref_pos[0], -ug_mm[-1], -vg_mm[-1]))
+
+    # create 2d image of the gnomonic projection because the gnomonic projection is bigger than the pattern
+    from pymicro.xray.detectors import RegArrayDetector2d
+    gnom = RegArrayDetector2d(size=np.array(detector.size))
+    gnom.ref_pos = detector.ref_pos
+    gnom.pixel_size = 1. / detector.pixel_size  # mm
+
+    gnom.data = np.zeros(gnom.size, dtype=np.uint8)
+    u_gnom = np.linspace(0, gnom.size[0] - 1, gnom.size[0])
+    v_gnom = np.linspace(0, gnom.size[1] - 1, gnom.size[1])
+    vv_gnom, uu_gnom = np.meshgrid(v_gnom, u_gnom)
+
+    # TODO use a function to go from mm to pixel coordinates
+    u_px = gnom.ucen + u_dif / gnom.pixel_size  # pixel, wrt (top left corner of gnom detector)
+    v_px = gnom.vcen + v_dif / gnom.pixel_size  # pixel, wrt (top left corner of gnom detector)
+    ug_px = gnom.ucen + ug_mm / gnom.pixel_size  # pixel, wrt (top left corner of gnom detector)
+    vg_px = gnom.vcen + vg_mm / gnom.pixel_size  # pixel, wrt (top left corner of gnom detector)
+    print(ug_px)
+    print(vg_px)
+
+    # remove pixel outside of the ROI (TODO use masked numpy array)
+    vg_px[ug_px > gnom.size[0] - 1] = gnom.vcen  # change vg_px first when testing on ug_px
+    ug_px[ug_px > gnom.size[0] - 1] = gnom.ucen
+    vg_px[ug_px < 0] = gnom.vcen
+    ug_px[ug_px < 0] = gnom.ucen
+
+    ug_px[vg_px > gnom.size[1] - 1] = gnom.ucen  # change ug_px first when testing on vg_px
+    vg_px[vg_px > gnom.size[1] - 1] = gnom.vcen
+    ug_px[vg_px < 0] = gnom.ucen
+    vg_px[vg_px < 0] = gnom.vcen
+
+    print(ug_px.min())
+    assert (ug_px.min() >= 0)
+    assert (ug_px.max() < gnom.size[0])
+    assert (vg_px.min() >= 0)
+    assert (vg_px.max() < gnom.size[1])
+
+    # convert pixel coordinates to integer
+    ug_px = ug_px.astype(np.uint16)
+    vg_px = vg_px.astype(np.uint16)
+    print('after conversion to uint16:')
+    print(ug_px)
+    print(vg_px)
+
+    gnom.data[ug_px, vg_px] = 1
+    return gnom
 
 if __name__ == '__main__':
     from matplotlib import pyplot as plt, cm, rcParams

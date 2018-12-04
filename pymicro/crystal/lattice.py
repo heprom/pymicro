030e3a748908f442a054a9237733ee981d470b3e
Alexiane Arnaud <alexiane.arnaud@mines-paristech.fr>
2018-12-04 09:02:59 +0100

    Get family new item

--- pymicro/crystal/lattice.py
+++ pymicro/crystal/lattice.py
@@ -1149,6 +1149,19 @@ class HklPlane(HklObject):
             family.append(HklPlane(-l, h, h, lattice))
             family.append(HklPlane(l, -h, h, lattice))
             family.append(HklPlane(l, h, -h, lattice))
+        elif len(np.unique(hkl_list)) == 2 and hkl_list[1] == hkl_list[2] and not '0' in hkl:
+            # 2 different ints, hll type -> 12 planes
+            family.append(HklPlane(h, l, l, lattice))
+            family.append(HklPlane(-h, l, l, lattice))
+            family.append(HklPlane(h, -l, l, lattice))
+            family.append(HklPlane(h, l, -l, lattice))
+            family.append(HklPlane(l, l, h, lattice))
+            family.append(HklPlane(-l, l, h, lattice))
+            family.append(HklPlane(l, -l, h, lattice))
+            family.append(HklPlane(l, l, -h, lattice))
+            family.append(HklPlane(l, h, l, lattice))
+            family.append(HklPlane(-l, h, l, lattice))
+            family.append(HklPlane(l, -h, l, lattice))
         elif len(np.unique(list(hkl))) == 3 and not '0' in hkl:
             # 3 different ints, all nonzeros -> 24 planes
             family.append(HklPlane(h, k, l, lattice))

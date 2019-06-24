29410938a7c9a10e9c28bd52af2c3add0b318c2d
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2019-06-24 18:00:40 +0200

    new Experiment class, with some tests

--- pymicro/xray/experiment.py
+++ pymicro/xray/experiment.py
@@ -1,25 +1,113 @@
 import json
-from pymicro.xray.detectors import Detector2d
+from json import JSONEncoder
+from pymicro.xray.detectors import Detector2d, RegArrayDetector2d
+
 
 class Sample:
 
-    def __init__(self, name):
+    def __init__(self, name=None):
+        self.name = name
+
+    def set_name(self, name):
         self.name = name
 
 class Experiment:
     """Class to represent an actual or virtual X-ray experiment.
     
-    A cartesion coordinate system (X, Y, Z) is associated with the experiment. By default X is the direction of X-rays 
-    and the sampel is placed at the origin (0, 0, 0).
+    A cartesian coordinate system (X, Y, Z) is associated with the experiment. By default X is the direction of X-rays 
+    and the sample is placed at the origin (0, 0, 0).
     """
 
     def __init__(self):
-        self.sample = Sample()
+        self.sample = Sample(name='dummy')
         self.detectors = []
+        self.active_detector_id = -1
 
-    def add_detector(self, detector):
-        assert detector.isinstance(Detector2d)
+    def set_sample(self, sample):
+        assert isinstance(sample, Sample) is True
+        self.sample = sample
+
+    def get_sample(self):
+        return self.sample
+
+    def add_detector(self, detector, set_as_active=True):
+        """Add a detector to this experiment.
+        
+        If this is the first detector, the active detector id is set accordingly.
+
+        :param Detector2d detector: an instance of the Detector2d class.
+        :param bool set_as_active: set this detector as active.
+        """
+        assert isinstance(detector, Detector2d) is True
         self.detectors.append(detector)
+        if set_as_active:
+            self.active_detector_id = self.get_number_of_detectors() - 1
 
     def get_number_of_detectors(self):
+        """Return the number of detector for this experiment."""
         return len(self.detectors)
+
+    def get_active_detector(self):
+        """Return the active detector for this experiment."""
+        return self.detectors[self.active_detector_id]
+
+    def save(self):
+        """Export the parameters to describe the current experiment to a file using json."""
+        dict_exp = {}
+        dict_exp['Sample'] = self.sample
+        dict_exp['Detectors'] = self.detectors
+        dict_exp['Active Detector Id'] = self.active_detector_id
+        # save to file using json
+        json_txt = json.dumps(dict_exp, indent=4, cls=ExperimentEncoder)
+        with open('experiment.txt', 'w') as f:
+            f.write(json_txt)
+
+    @staticmethod
+    def load(file_path='experiment.txt'):
+        with open(file_path, 'r') as f:
+            dict_exp = json.load(f)
+        sample = Sample()
+        sample.set_name(dict_exp['Sample']['Name'])
+        exp = Experiment()
+        exp.set_sample(sample)
+        for i in range(len(dict_exp['Detectors'])):
+            dict_det = dict_exp['Detectors'][i]
+            if dict_det['Class'] == 'Detector2d':
+                det = Detector2d(size=dict_det['Size (pixels)'])
+                det.ref_pos = dict_det['Reference Position (mm)']
+            if dict_det['Class'] == 'RegArrayDetector2d':
+                det = RegArrayDetector2d(size=dict_det['Size (pixels)'])
+                det.pixel_size = dict_det['Pixel Size (mm)']
+                det.ref_pos = dict_det['Reference Position (mm)']
+                import numpy as np
+                det.u_dir = np.array(dict_det['u_dir'])
+                det.v_dir = np.array(dict_det['v_dir'])
+                det.w_dir = np.array(dict_det['w_dir'])
+        return exp
+
+
+class ExperimentEncoder(json.JSONEncoder):
+
+    def default(self, o):
+        if isinstance(o, Sample):
+            dict_sample = {}
+            dict_sample['Name'] = o.name
+            return dict_sample
+        if isinstance(o, RegArrayDetector2d):
+            dict_det = {}
+            dict_det['Class'] = o.__class__.__name__
+            dict_det['Size (pixels)'] = o.size
+            dict_det['Pixel Size (mm)'] = o.pixel_size
+            dict_det['Data Type'] = str(o.data_type)
+            dict_det['Reference Position (mm)'] = o.ref_pos.tolist()
+            dict_det['u_dir'] = o.u_dir.tolist()
+            dict_det['v_dir'] = o.v_dir.tolist()
+            dict_det['w_dir'] = o.w_dir.tolist()
+            return dict_det
+        if isinstance(o, Detector2d):
+            dict_det = {}
+            dict_det['Class'] = o.__class__.__name__
+            dict_det['Size (pixels)'] = o.size
+            dict_det['Data Type'] = o.data_type
+            dict_det['Reference Position (mm)'] = o.ref_pos.tolist()
+            return dict_det

50e244bf6c25167c7df4878d6107e4fc1288bf8f
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2019-06-27 13:05:45 +0200

    added sample and geometry to an Experiment

--- pymicro/xray/experiment.py
+++ pymicro/xray/experiment.py
@@ -1,18 +1,64 @@
 import json
 from json import JSONEncoder
+import numpy as np
 from pymicro.xray.detectors import Detector2d, RegArrayDetector2d
 
+class ObjectGeometry:
+    """Class to represent any object geometry.
+    
+    The geometry may have multiple form, including just a point, a regular 3D array or it may be described by a CAD 
+    file using the STL format."""
+
+    def __init__(self, geo_type='point'):
+        self.set_type(geo_type)
+
+    def set_type(self, geo_type):
+        assert (geo_type in ['point', 'array', 'cad']) is True
+        self.geo_type = geo_type
+
+    def get_bounding_box(self):
+        if self.geo_type == 'point':
+            return (0., 0., 0.), (0., 0., 0.)
+        elif self.geo_type == 'array':
+            return (0., 0., 0.), self.array.shape
+        elif self.geo_type == 'cad':
+            bounds = self.cad.GetBounds()
+            return (bounds[0], bounds[2], bounds[4]), (bounds[1], bounds[3], bounds[5])
 
 class Sample:
+    """Class to describe a material sample.
+    
+    A sample is made by a given material (that may have multiple phases), has a name and a position in the experimental 
+    local frame. A sample also have a geometry (just a point by default), that may be used to discretize the volume 
+    in space or display it in 3D.
+    
+    .. note::
 
-    def __init__(self, name=None):
+      For the moment, the material is simply a crystal lattice.
+    """
+
+    def __init__(self, name=None, position=(0., 0., 0.), geo=None, ):
         self.name = name
+        self.set_position(position)
+        if geo is None:
+            geo = ObjectGeometry()
+        self.set_geometry(geo)
 
     def set_name(self, name):
         self.name = name
 
+    def set_position(self, position):
+        self.position = np.array(position)
+
+    def set_geometry(self, geo):
+        if geo is None:
+            geo = ObjectGeometry()
+        assert isinstance(geo, ObjectGeometry) is True
+        self.geo = geo
+
+
 class Experiment:
-    """Class to represent an actual or virtual X-ray experiment.
+    """Class to represent an actual or a virtual X-ray experiment.
     
     A cartesian coordinate system (X, Y, Z) is associated with the experiment. By default X is the direction of X-rays 
     and the sample is placed at the origin (0, 0, 0).
@@ -68,6 +114,11 @@ class Experiment:
             dict_exp = json.load(f)
         sample = Sample()
         sample.set_name(dict_exp['Sample']['Name'])
+        sample.set_position(dict_exp['Sample']['Position'])
+        if dict_exp['Sample'].has_key('Geometry'):
+            sample_geo = ObjectGeometry()
+            sample_geo.set_type(dict_exp['Sample']['Geometry']['Type'])
+            sample.set_geometry(sample_geo)
         exp = Experiment()
         exp.set_sample(sample)
         for i in range(len(dict_exp['Detectors'])):
@@ -89,9 +140,15 @@ class Experiment:
 class ExperimentEncoder(json.JSONEncoder):
 
     def default(self, o):
+        if isinstance(o, ObjectGeometry):
+            dict_geo = {}
+            dict_geo['Type'] = o.geo_type
+            return dict_geo
         if isinstance(o, Sample):
             dict_sample = {}
             dict_sample['Name'] = o.name
+            dict_sample['Position'] = o.position
+            dict_sample['Geometry'] = o.geo
             return dict_sample
         if isinstance(o, RegArrayDetector2d):
             dict_det = {}

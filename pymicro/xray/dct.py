476c0ff711a70d591e5658d50f91445f92e43bd3
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2020-04-10 10:55:54 +0200

    made a new class DctForwardSimulation, and updated Experiment code

--- pymicro/xray/dct.py
+++ pymicro/xray/dct.py
@@ -1,63 +1,245 @@
 """The dct module provide helpers functions to work with experimental diffraction contrast tomography data.
 """
 import os
+import h5py
 import numpy as np
 from scipy import ndimage
-from skimage.transform import radon
 from matplotlib import pyplot as plt, cm
-from pymicro.crystal.microstructure import Grain
+from pymicro.xray.experiment import ForwardSimulation, Experiment
 from pymicro.crystal.lattice import HklPlane
-from pymicro.xray.xray_utils import lambda_keV_to_nm, radiograph
+from pymicro.xray.xray_utils import lambda_keV_to_nm, radiograph, radiographs
+from pymicro.crystal.microstructure import Grain, Orientation
 
 
-def dct_projection(orientations, data, dif_grains, omega, lambda_keV, detector, lattice, include_direct_beam=True,
-                   att=5, verbose=True):
-    '''Work in progress, will replace function in the microstructure module.'''
-    full_proj = np.zeros(detector.size, dtype=np.float)
-    lambda_nm = lambda_keV_to_nm(lambda_keV)
-    omegar = omega * np.pi / 180
-    R = np.array([[np.cos(omegar), -np.sin(omegar), 0], [np.sin(omegar), np.cos(omegar), 0], [0, 0, 1]])
+class DctForwardSimulation(ForwardSimulation):
+    """Class to represent a Forward Simulation."""
 
-    if include_direct_beam:
-        # add the direct beam part by computing the radiograph of the sample without the diffracting grains
-        data_abs = np.where(data > 0, 1, 0)
+    def __init__(self, verbose=False):
+        super(DctForwardSimulation, self).__init__('dct', verbose=verbose)
+        self.hkl_planes = []
+        self.check = 1  # grain id to display infos in verbose mode
+        self.omegas = None
+        self.reflections = []
+        self.exp = Experiment()
+
+    def set_experiment(self, experiment):
+        """Attach an X-ray experiment to this simulation."""
+        self.exp = experiment
+
+    def set_hkl_planes(self, hkl_planes):
+        self.hkl_planes = hkl_planes
+
+    def set_diffracting_famillies(self, hkl_list):
+        """Set the list of diffracting hk planes using a set of families."""
+        symmetry = self.exp.get_sample().get_material().get_symmetry()
+        hkl_planes = []
+        for hkl in hkl_list:
+            # here we set include_friedel_pairs to False as we take it into account in the calculation
+            planes = HklPlane.get_family(hkl, include_friedel_pairs=True, crystal_structure=symmetry)
+            for plane in planes:  # fix the lattice
+                plane.set_lattice(self.exp.get_sample().get_material())
+            hkl_planes.extend(planes)
+        self.set_hkl_planes(hkl_planes)
+
+
+    def setup(self, omega_step):
+        """Setup the forward simulation."""
+        assert self.exp.source.min_energy == self.exp.source.max_energy  # monochromatic case
+        lambda_keV = self.exp.source.max_energy
+        self.omegas = np.linspace(0.0, 360.0, num=int(360.0 / omega_step), endpoint=False)
+        self.reflections = []
+        for omega in self.omegas:
+            self.reflections.append([])
+        for g in self.exp.sample.microstructure.grains:
+            for plane in self.hkl_planes:
+                (h, k, i, l) = HklPlane.three_to_four_indices(*plane.miller_indices())
+                try:
+                    (w1, w2) = g.dct_omega_angles(plane, lambda_keV, verbose=False)
+                except ValueError:
+                    if self.verbose:
+                        print('plane {} does not fulfil the Bragg condition for grain {:d}'.format((h, k, i, l), g.id))
+                    continue
+                # add angles for Friedel pairs
+                w3 = (w1 + 180.) % 360
+                w4 = (w2 + 180.) % 360
+                if self.verbose and g.id == self.check:
+                    print('grain %d, angles for plane %d%d%d: w1=%.3f and w2=%.3f | delta=%.1f' % (g.id, h, k, l, w1, w2, w1-w2))
+                    print('(%3d, %3d, %3d, %3d) -- %6.2f & %6.2f' % (h, k, i, l, w1, w2))
+                self.reflections[int(w1 / omega_step)].append([g.id, (h, k, l)])
+                self.reflections[int(w2 / omega_step)].append([g.id, (h, k, l)])
+                self.reflections[int(w3 / omega_step)].append([g.id, (-h, -k, -l)])
+                self.reflections[int(w4 / omega_step)].append([g.id, (-h, -k, -l)])
+
+    def load_grain(self, gid=1):
+        print('loading grain from file 4_grains/phase_01/grain_%04d.mat' % gid)
+        with h5py.File(os.path.join(self.exp.get_sample().data_dir, '4_grains/phase_01/grain_%04d.mat' % gid)) as gmat:
+            g = Grain(gid, Orientation.from_rodrigues(gmat['R_vector'][()]))
+            g.om_exp = gmat['om_exp'][0, :]
+            g.uv_exp = gmat['uv_exp'][:, :]
+            g.center = gmat['center'][:, 0]
+            g.position = gmat['center'][:, 0]
+            try:
+                ref_included = gmat['proj/included'][0][0]
+                g.included = gmat[ref_included][0, :]
+                ref_ondet = gmat['proj/ondet'][0][0]
+                g.ondet = gmat[ref_ondet][0, :]
+                # grab the projection stack
+                ref_stack = gmat['proj']['stack'][0][0]
+                g.stack_exp = gmat[ref_stack][()].transpose(1, 2, 0)  # now in [ndx, u, v] form
+                g.hklsp = gmat['allblobs/hklsp'][:, :]
+            except AttributeError:
+                # classic file organization
+                g.included = gmat['proj/included'][0, :]
+                g.ondet = gmat['proj/ondet'][0, :]
+                g.stack_exp = gmat['proj/stack'][()].transpose(1, 2, 0)  # now in [ndx, u, v] form
+                # for this data set, we have to hack around the DCT + TT work in progress
+                ref_hklsp = gmat['allblobs/hklsp'][()][0][0]
+                g.hklsp = gmat[ref_hklsp][:, :]
+        self.grain = g
+        if self.verbose:
+            print('experimental proj stack shape: {}'.format(g.stack_exp.shape))
+
+    def grain_projection_image(self, g_uv, g_proj):
+        """Produce a 2D image placing all diffraction spots of a given grain at their respective position on the detector.
+
+        Spots outside the detector are are skipped while those only partially on the detector are cropped accordingly.
+
+        :param g_proj: image stack of the diffraction spots. The first axis is so that g_proj[0] is the first spot \
+        the second axis is the horizontal coordinate of the detector (u) and the third axis the vertical coordinate \
+        of the detector (v).
+        :param g_uv: list or array of the diffraction spot position.
+        :returns: a 2D composite image of all the diffraction spots.
+        """
+        print(len(g_proj), g_uv.shape[1])
+        assert len(g_proj) == g_uv.shape[1]
+        image = np.zeros(self.exp.get_active_detector().get_size_px())
+        for i in range(len(g_proj)):
+            spot = g_proj[i]
+            if self.verbose:
+                print('i={0}, size of spot: {1}'.format(i, spot.shape))
+                print('placing diffraction spot at location {0}'.format(g_uv[:, i]))
+            add_to_image(image, spot, g_uv[:, i], self.verbose)
+        return image
+
+    def grain_projection_exp(self, gid=1):
+        """Produce a composite image with all the experimental diffraction spots of this grain on the detector.
+
+        :param int gid: the number of the selected grain.
+        :returns: a 2D composite image of all the diffraction spots.
+        """
+        #self.grain = self.exp.get_sample().get_microstructure().get_grain(gid)
+        if not hasattr(self, 'grain') or self.grain.id != gid:
+            # load the corresponding grain
+            self.load_grain(gid=gid)
+        return self.grain_projection_image(self.grain.uv_exp, self.grain.stack_exp)
+
+    def grain_projection_simulation(self, gid=1, data=None):
+        """Function to compute all the grain projection in DCT geometry and create a composite image."""
+        print('forward simulation of grain %d' % gid)
+        detector = self.exp.get_active_detector()
+        lambda_keV = self.exp.source.max_energy
+        lambda_nm = lambda_keV_to_nm(lambda_keV)
+        X = np.array([1., 0., 0.]) / lambda_nm
+        lattice = self.exp.get_sample().get_material()
+
+        if not hasattr(self, 'grain'):
+            # load the corresponding grain
+            self.load_grain(gid=gid)
+        from scipy import ndimage
+        if data is None:
+            grain_ids = self.exp.get_sample().get_grain_ids()
+            print('binarizing grain %d' % gid)
+            data = np.where(grain_ids[ndimage.find_objects(grain_ids == gid)[0]] == gid, 1, 0)
+        print('shape of binary grain is {}'.format(data.shape))
+
+        # compute all the omega values
+        print('simulating diffraction spot positions on the detector')
+        omegas = np.zeros(2 * len(self.hkl_planes))
+        g_uv = np.zeros((2, 2 * len(self.hkl_planes)))
+        for i, plane in enumerate(self.hkl_planes):
+            #print(plane.miller_indices())
+            try:
+                w1, w2 = self.grain.dct_omega_angles(plane, lambda_keV, verbose=False)
+            except ValueError:
+                # plane does not fulfil the Bragg condition
+                continue
+            omegas[2 * i] = w1
+            omegas[2 * i + 1] = w2
+            for j in range(2):
+                omega = omegas[2 * i + j]
+                omegar = omega * np.pi / 180
+                R = np.array([[np.cos(omegar), -np.sin(omegar), 0], [np.sin(omegar), np.cos(omegar), 0], [0, 0, 1]])
+                gt = self.grain.orientation_matrix().transpose()
+                G = np.dot(R, np.dot(gt, plane.scattering_vector()))
+                K = X + G
+                # position of the grain at this rotation angle
+                g_pos_rot = np.dot(R, self.grain.position)
+                pg = detector.project_along_direction(K, g_pos_rot)
+                (up, vp) = detector.lab_to_pixel(pg)[0]
+                g_uv[:, 2 * i + j] = up, vp
+        # build the 3d projection stack at once
+        print('building grain projections stack')
+        stack_sim = radiographs(data, omegas)
+        stack_sim = stack_sim.transpose(2, 0, 1)[:, :, ::-1]  # (u, v) axes correspond to (Y, -Z) for DCT detector
+        return self.grain_projection_image(g_uv, stack_sim)
+
+
+    def dct_projection(self, omega, include_direct_beam=True, att=5):
+        """Function to compute a full DCT projection at a given omega angle."""
+        if len(self.reflections) == 0:
+            print('empty list of reflections, you should run the setup function first')
+            return None
+        grain_ids = self.exp.get_sample().get_grain_ids()
+        detector = self.exp.get_active_detector()
+        lambda_keV = self.exp.source.max_energy
+        lattice = self.exp.get_sample().get_material()
+        index = np.argmax(self.omegas > omega)
+        dif_grains = self.reflections[index - 1]  # grains diffracting between omegas[index - 1] and omegas[index]
+        # intialize image result
+        full_proj = np.zeros(detector.get_size_px(), dtype=np.float)
+        lambda_nm = lambda_keV_to_nm(lambda_keV)
+        omegar = omega * np.pi / 180
+        R = np.array([[np.cos(omegar), -np.sin(omegar), 0], [np.sin(omegar), np.cos(omegar), 0], [0, 0, 1]])
+
+        if include_direct_beam:
+            # add the direct beam part by computing the radiograph of the sample without the diffracting grains
+            data_abs = np.where(grain_ids > 0, 1, 0)
+            for (gid, (h, k, l)) in dif_grains:
+                mask_dif = (grain_ids == gid)
+                data_abs[mask_dif] = 0  # remove this grain from the absorption
+            proj = radiograph(data_abs, omega)[:, ::-1]  # (u, v) axes correspond to (Y, -Z) for DCT detector
+            add_to_image(full_proj, proj / att, np.array(full_proj.shape) // 2)
+
+        # add diffraction spots
+        X = np.array([1., 0., 0.]) / lambda_nm
         for (gid, (h, k, l)) in dif_grains:
-            mask_dif = (data == gid)
-            data_abs[mask_dif] = 0  # remove this grain from the absorption
-        proj = radiograph(data_abs, omega)
-        add_to_image(full_proj, proj[::-1, ::-1] / att, np.array(full_proj.shape) // 2)
-
-    # add diffraction spots
-    X = np.array([1., 0., 0.]) / lambda_nm
-    for (gid, (h, k, l)) in dif_grains:
-        grain_data = np.where(data == gid, 1, 0)
-        if np.sum(grain_data) < 1:
-            print('skipping grain %d' % gid)
-            continue
-        local_com = np.array(ndimage.measurements.center_of_mass(grain_data, data))
-        print('local center of mass (voxel): {0}'.format(local_com))
-        g_center_mm = detector.pixel_size * (local_com - 0.5 * np.array(data.shape))
-        print('center of mass (voxel): {0}'.format(local_com - 0.5 * np.array(data.shape)))
-        print('center of mass (mm): {0}'.format(g_center_mm))
-        # compute scattering vector
-        gt = orientations[gid].orientation_matrix().transpose()
-        # gt = micro.get_grain(gid).orientation_matrix().transpose()
-        p = HklPlane(h, k, l, lattice)
-        G = np.dot(R, np.dot(gt, p.scattering_vector()))
-        K = X + G
-        # position of the grain at this rotation
-        g_pos_rot = np.dot(R, g_center_mm)
-        pg = detector.project_along_direction(K, g_pos_rot)
-        (up, vp) = detector.lab_to_pixel(pg)
-        if verbose:
-            print('\n* gid=%d, (%d,%d,%d) plane, angle=%.1f' % (gid, h, k, l, omega))
-            print('diffraction vector:', K)
-            print('postion of the grain at omega=%.1f is ' % omega, g_pos_rot)
-            print('up=%d, vp=%d for plane (%d,%d,%d)' % (up, vp, h, k, l))
-        data_dif = grain_data[ndimage.find_objects(data == gid)[0]]
-        proj_dif = radiograph(data_dif, omega)  # (Y, Z) coordinate system
-        add_to_image(full_proj, proj_dif[::-1, ::-1], (up, vp), verbose)  # (u, v) axes correspond to (-Y, -Z)
-    return full_proj
+            grain_data = np.where(grain_ids == gid, 1, 0)
+            if np.sum(grain_data) < 1:
+                print('skipping grain %d' % gid)
+                continue
+            local_com = np.array(ndimage.measurements.center_of_mass(grain_data, grain_ids))
+            print('local center of mass (voxel): {0}'.format(local_com))
+            g_center_mm = detector.get_pixel_size() * (local_com - 0.5 * np.array(grain_ids.shape))
+            print('center of mass (voxel): {0}'.format(local_com - 0.5 * np.array(grain_ids.shape)))
+            print('center of mass (mm): {0}'.format(g_center_mm))
+            # compute scattering vector
+            gt = self.exp.get_sample().get_microstructure().get_grain(gid).orientation_matrix().transpose()
+            p = HklPlane(h, k, l, lattice)
+            G = np.dot(R, np.dot(gt, p.scattering_vector()))
+            K = X + G
+            # position of the grain at this rotation angle
+            g_pos_rot = np.dot(R, g_center_mm)
+            pg = detector.project_along_direction(K, g_pos_rot)
+            (up, vp) = detector.lab_to_pixel(pg)[0]
+            if self.verbose:
+                print('\n* gid=%d, (%d,%d,%d) plane, angle=%.1f' % (gid, h, k, l, omega))
+                print('diffraction vector:', K)
+                print('postion of the grain at omega=%.1f is ' % omega, g_pos_rot)
+                print('up=%d, vp=%d for plane (%d,%d,%d)' % (up, vp, h, k, l))
+            data_dif = grain_data[ndimage.find_objects(grain_ids == gid)[0]]
+            proj_dif = radiograph(data_dif, omega)  # (Y, Z) coordinate system
+            add_to_image(full_proj, proj_dif[:, ::-1], (up, vp), self.verbose)  # (u, v) axes correspond to (Y, -Z)
+        return full_proj
 
 
 def add_to_image(image, inset, uv, verbose=False):

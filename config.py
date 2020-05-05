57eaa7f8515dc06f55d561eded94467abe256cdb
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2020-05-05 16:45:47 +0200

    add a new config file at the project root

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+PYMICRO_ROOT_DIR = os.path.abspath(os.curdir)
+PYMICRO_EXAMPLES_DATA_DIR = os.path.join(PYMICRO_ROOT_DIR, 'examples', 'data')
+PYMICRO_XRAY_DATA_DIR = os.path.join(PYMICRO_ROOT_DIR, 'pymicro', 'xray', 'data')

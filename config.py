8e34f3e7efb2367d8ea824931a75eefc5e5aafe9
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2020-09-11 09:08:47 +0200

    fix path issue when running from outside pymicro

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-PYMICRO_ROOT_DIR = os.path.abspath(os.curdir)
+PYMICRO_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 PYMICRO_EXAMPLES_DATA_DIR = os.path.join(PYMICRO_ROOT_DIR, 'examples', 'data')
 PYMICRO_XRAY_DATA_DIR = os.path.join(PYMICRO_ROOT_DIR, 'pymicro', 'xray', 'data')

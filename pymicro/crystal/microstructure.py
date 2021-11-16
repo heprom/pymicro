bff3e8ff678cb0fb50a894c40ee42e9a3eb907f9
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2021-11-16 15:21:58 +0100

    account for non isotropic voxel_size in from_neper

--- pymicro/crystal/microstructure.py
+++ pymicro/crystal/microstructure.py
@@ -4136,7 +4136,7 @@ class Microstructure(SampleData):
             print(data.shape)
             assert np.prod(dims) == data.shape[0]
             micro.set_grain_map(data.reshape(dims[::-1]).transpose(2, 1, 0),
-                                voxel_size=voxel_size[0])  # swap X/Z axes
+                                voxel_size=voxel_size)  # swap X/Z axes
             print('updating grain data table and grain geometry')
             micro.sync_grain_table_with_grain_map(sync_geometry=True)
             # if necessary set the phase_map

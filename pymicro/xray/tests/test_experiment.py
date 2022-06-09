8629029c5e87615dee0da18181647e2b79cad639
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2022-06-09 22:21:33 +0200

    fix the tests after last update

--- pymicro/xray/tests/test_experiment.py
+++ pymicro/xray/tests/test_experiment.py
@@ -9,18 +9,20 @@ class ExperimentTests(unittest.TestCase):
         """testing the experiment module:"""
         self.experiment = Experiment()
         self.experiment.get_sample().set_name('test sample')
+        self.experiment.get_sample().autodelete = True
         self.experiment.get_source().set_max_energy(120.)
-        print(self.experiment.get_sample().geo.geo_type)
 
     def test_add_detector(self):
+        """Test the add_detector method."""
         detector = RegArrayDetector2d(size=(512, 512))
         self.assertEqual(self.experiment.get_number_of_detectors(), 0)
         self.experiment.add_detector(detector)
         self.assertEqual(self.experiment.get_number_of_detectors(), 1)
-        self.assertTrue(self.experiment.sample.microstructure.autodelete)
+        self.assertTrue(self.experiment.sample.autodelete)
         del self.experiment
 
     def test_save(self):
+        """Test the save method for an experiment."""
         if os.path.exists('experiment.txt'):
             os.remove('experiment.txt')
         detector1 = RegArrayDetector2d(size=(512, 512))
@@ -39,3 +41,11 @@ class ExperimentTests(unittest.TestCase):
         del self.experiment
         exp = Experiment.load('experiment.txt')
         self.assertTrue(exp.get_source().max_energy == 120.)
+        del exp
+
+    def tearDown(self):
+        if os.path.exists('experiment.txt'):
+            os.remove('experiment.txt')
+        if os.path.exists('dummy_data.h5'):
+            os.remove('dummy_data.h5')
+            os.remove('dummy_data.xdmf')

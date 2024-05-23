03abb31296a3f5d3e9aa2b258a35968d03d9089a
Basile Marchand <basile.marchand@mat.mines-paristech.fr>
2024-05-23 15:11:11 +0200

    Add download_data function for caching and downloading files for examples

--- pymicro/__init__.py
+++ pymicro/__init__.py
@@ -5,3 +5,34 @@
 """
 
 __version__ = '0.5.3'
+
+import pathlib as pl 
+import os 
+import requests
+
+DATASET_SOURCE = "https://raw.githubusercontent.com/heprom/pymicro-data/main"
+
+def get_cache_dir(data_home=None):
+    if data_home is None:
+        data_home = os.environ.get("PYMICRO_DATA", pl.Path.home() / ".pymicro_data")
+    data_home = pl.Path(data_home)
+    if not data_home.exists():
+        data_home.mkdir()
+    return data_home
+
+def download_data( fname, cache=True):
+    data_home = get_cache_dir()
+    data_file = data_home / fname
+    if not data_file.exists() or not cache:
+        data_file.parent.mkdir(parents=True, exist_ok=True)
+        url = f"{DATASET_SOURCE}/{fname}"
+        print(f"Downloading {url} to {data_file}")
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise ValueError(f"Could not download {url}")
+
+        with open(data_file, "wb") as fid:
+            fid.write(response.content)
+    else:
+        print(f"Use cached file {data_file}")
+    return data_file

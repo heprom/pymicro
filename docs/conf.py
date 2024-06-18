bb00bc9f72224d8837091da51a11d4ad9d3fe197
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2024-06-18 14:19:49 +0200

    work around mock issue to build doc

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+'''
 import mock
 
 MOCK_MODULES = ['numpy', 'scipy', 'scipy.special', 'matplotlib',
@@ -32,6 +33,7 @@ MOCK_MODULES = ['numpy', 'scipy', 'scipy.special', 'matplotlib',
 # , 'ImPanel', 'PlotPanel']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
+'''
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.

b063db23ffd3f4b7ca42d01b5d514d45b1b5468d
Henry Proudhon <henry.proudhon@mines-paristech.fr>
2024-08-27 10:02:19 +0200

    fix graph test

--- pymicro/crystal/tests/test_Graph.py
+++ pymicro/crystal/tests/test_Graph.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 from pymicro.crystal.microstructure import Microstructure
+from pymicro.crystal.graph import create_graph
 from config import PYMICRO_EXAMPLES_DATA_DIR
 
 
@@ -9,9 +10,9 @@ class GraphTests(unittest.TestCase):
     def setUp(self):
         print('testing the Microstructure graph utilities')
         
-    def test_graph(self):
+    def test_create_graph(self):
         m = Microstructure(os.path.join(PYMICRO_EXAMPLES_DATA_DIR, 'm1_data.h5'))
-        rag = m.graph()
+        rag = create_graph(m)
         self.assertEqual(len(rag.nodes), m.get_number_of_grains())
         e = rag.edges[15, 26]  # pick two neighboring grains
         self.assertTrue('misorientation' in e)
